732b99aa3ae6c26b909dba2b040c7dd838cf826c
Luis Doriz <luisdoriz@Luiss-MacBook-Pro.local>
2021-05-20 14:28:08 -0500

    dasds

--- actions/patients/read.js
+++ actions/patients/read.js
@@ -4,6 +4,7 @@ const { Patient } = models;
 
 const findPatientsByQuery = async (query) => Patient.findAll({
     where: query,
+    order: [["id", "DESC"]],
 });
 
 const findPatientByQuery = async (query) => Patient.findOne({
