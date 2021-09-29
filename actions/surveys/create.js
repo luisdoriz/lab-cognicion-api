28f737e94b96ceb44d674884f768841840de873e
Juan Manuel Alvarez <juanmanuel@disrupcion.mx>
2021-09-29 10:45:06 -0500

    fixed error in create survey

--- actions/surveys/create.js
+++ actions/surveys/create.js
@@ -1,38 +1,27 @@
-const { Survey } = require('../../models');
-const { createAccessUrl } = require('../accessUrls/create');
+const { Survey } = require("../../models");
+const { createAccessUrl } = require("../accessUrls/create");
 
-const postSurvey = async (body) =>  Survey.create(body);
+const postSurvey = async (body) => Survey.create(body);
 
-const createSurvey = async (body) =>  {
-    try {
-        const accessUrl = await createAccessUrl()
-        idAccessUrl = accessUrl.id
-        const {
-            idUser,
-            idPatient,
-            idSurveyType: type,
-            name,
-            lastName,
-            relationship,
-        } = body;
-        const surveyBody = {
-            idUser,
-            idPatient,
-            type,
-            name,
-            lastName,
-            relationship,
-            idAccessUrl,
-        }
-        test = await postSurvey(surveyBody)
-        return test
-    } catch(err) {
-        console.log("error", err)
-        return null
-    }
+const createSurvey = async (body) => {
+  try {
+    const accessUrl = await createAccessUrl();
+    idAccessUrl = accessUrl.id;
+    const { idUser, idPatient, idSurveyType: type } = body;
+    const surveyBody = {
+      idUser,
+      idPatient,
+      type,
+      idAccessUrl,
+    };
+    test = await postSurvey(surveyBody);
+    return test;
+  } catch (err) {
+    console.log("error", err);
+    return null;
+  }
 };
 
-
 module.exports = {
-    createSurvey,
-  };
+  createSurvey,
+};
